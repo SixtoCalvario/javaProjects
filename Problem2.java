1b0b715dc6201dfbf2d4b03394b11fa13883be03
SixtoCalvario <131615750+SixtoCalvario@users.noreply.github.com>
2024-08-20 05:32:49 +0000

    yuigh

--- Problem2.java
+++ Problem2.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 public class Problem02{
-    
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); 
         // take the first 3 inputs to store the cell data
@@ -11,7 +11,6 @@ public class Problem02{
             System.out.print("Enter Cell Data: ");
             XYZ[i] = input.nextLine().toUpperCase();
         }
-        
         // the next 6 lines of input are for operations
         String operation = "";
         for (int i = 0; i < 6; i++) {
@@ -19,51 +18,44 @@ public class Problem02{
             operation = input.nextLine().toUpperCase();
             System.out.println(operator(XYZ, operation));
         }
-
     }
 
 
     // divides cell into 2 cells 
-    public static String theDivider(String[] letters, int cellChoice) {
-        String cell1 = (letters[cellChoice]).substring(0,4)+(letters[cellChoice]).substring(0,4);
-        String cell2 = (letters[cellChoice]).substring(4)+(letters[cellChoice]).substring(4);
+    public static String divide(String cellChoice) {
+        String cell1 = cellChoice.substring(0,4)+cellChoice.substring(0,4);
+        String cell2 = cellChoice.substring(4)+cellChoice.substring(4);
         return cell1+" and "+cell2;
     }
 
 
     // ADDn
-    public static String add(String[] letters, int n, int cellChoice) {
-        int m = ((letters[cellChoice]).length())-n;
-        String firstBit = (letters[cellChoice]).substring(0,(n));
-        return firstBit+(letters[cellChoice]).substring(0,m);
+    public static String add(String cellChoice, int n) {
+        int m = cellChoice.length()-n;
+        String firstBit = cellChoice.substring(0,(n));
+        return firstBit+cellChoice.substring(0,m);
     }
 
 
     // SUBTRACTn
-    public static String subtract(String[] letters, int n, int cellChoice) {
-        String cell = (letters[cellChoice]);
-        String lastBit = cell.substring(cell.length()-n);
-        String missingFirstBit = cell.substring(n);
+    public static String subtract(String cellChoice, int n) {
+        String lastBit = cellChoice.substring(cellChoice.length()-n);
+        String missingFirstBit = cellChoice.substring(n);
         String sortedLastBit = alphabetize(lastBit);
         return missingFirstBit+sortedLastBit;
     }
 
     // UNION
-    public static String union(String[] letters, int cellChoice1, int cellChoice2) {
-        String cell1 = letters[cellChoice1];
-        String cell2 = letters[cellChoice2]; 
+    public static String union(String cell1, String cell2) {
         // final 4 bits of cell 1
         String lastBits =  alphabetize(cell1.substring(4)) ;
         // first 4 bits of cell2
         String firstBits = alphabetize(cell2.substring(0,4));
         return lastBits+firstBits;
-
     }
 
     // INTERSECT
-    public static String intersect(String[] letters, int cellChoice1, int cellChoice2) {
-        String cell1 = letters[cellChoice1];
-        String cell2 = letters[cellChoice2];
+    public static String intersect(String cell1, String cell2) {
         String part1 = alphabetize(cell1.substring(0,2)+cell1.substring(6));
         String part2 = alphabetize(cell2.substring(0,2)+cell2.substring(6));
         return part1+part2;
@@ -86,15 +78,117 @@ public class Problem02{
     
     public static String operator(String[] letters, String userInput) {
         String[] inputArray = userInput.split(" ");
-        
-        if( (inputArray.length) == 2 ) {
-            return subOperator(inputArray[0], inputArray[1]);
-        }
+        String cell1 = "";
+        String cell2 = "";
 
-
+        // reading array from back to start
         for (int i = (inputArray.length-1); i>=0; i--) {
-            // if 
+            
+            if(inputArray[i].contains("ADD")) {
+                cell2 = inputArray[i+1];
+                if(cell2.equals("X")) {
+                    cell2 = letters[0];
+                }
+                if(cell2.equals("Y")){
+                    cell2 = letters[1];
+                }
+                if(cell2.equals("Z")) {
+                    cell2 = letters[2];
+                }
+                int n = Integer.parseInt(""+(inputArray[i].charAt(inputArray[i].length()-1)));
+                cell2 = add(cell2, n);
+            }
 
+
+            if(inputArray[i].contains("SUB")) {
+                cell2 = inputArray[i+1];
+                if(cell2.equals("X")) {
+                    cell2 = letters[0];
+                }
+                if(cell2.equals("Y")){
+                    cell2 = letters[1];
+                }
+                if(cell2.equals("Z")) {
+                    cell2 = letters[2];
+                }
+                int n = Integer.parseInt(""+(inputArray[i].charAt(inputArray[i].length()-1)));
+                cell2 = subtract(cell2, n);
+            }
+
+
+            if(inputArray[i].contains("UNI")) {
+                // this is done when there are no other operations
+                if(inputArray.length==3){
+                    if(inputArray[1].equals("X")) {
+                        cell1 = letters[0];
+                    }
+                    if(inputArray[1].equals("Y")) {
+                        cell1 = letters[1];
+                    }
+                    if(inputArray[1].equals("Z")) {
+                        cell1 = letters[2];
+                    }
+
+                    if(inputArray[2].equals("X")) {
+                        cell2 = letters[0];
+                    }
+                    if(inputArray[2].equals("Y")) {
+                        cell2 = letters[1];
+                    }
+                    if(inputArray[2].equals("Z")) {
+                        cell2 = letters[2];
+                    }
+                    cell1 = union(cell1, cell2);
+                }
+                // this is done when there is other operations performed
+                else{
+                    cell1 = union(cell1, cell2);
+                }
+            }
+
+            if(inputArray[i].contains("INT")) {
+                // this is done when there are no other operations
+                if(inputArray.length==3){
+                    if(inputArray[1].equals("X")) {
+                        cell1 = letters[0];
+                    }
+                    if(inputArray[1].equals("Y")) {
+                        cell1 = letters[1];
+                    }
+                    if(inputArray[1].equals("Z")) {
+                        cell1 = letters[2];
+                    }
+
+                    if(inputArray[2].equals("X")) {
+                        cell2 = letters[0];
+                    }
+                    if(inputArray[2].equals("Y")) {
+                        cell2 = letters[1];
+                    }
+                    if(inputArray[2].equals("Z")) {
+                        cell2 = letters[2];
+                    }
+                    cell1 = intersect(cell1, cell2);
+                }
+                // this is done when there is other operations performed
+                else{
+                    cell1 = intersect(cell1, cell2);
+                }
+            }
+
+            if(inputArray[i].contains("DIV")) {
+                cell1 = inputArray[i+1];
+                if(cell1.equals("X")) {
+                    cell2 = letters[0];
+                }
+                if(cell1.equals("Y")){
+                    cell2 = letters[1];
+                }
+                if(cell1.equals("Z")) {
+                    cell2 = letters[2];
+                }
+                cell2 = divide(cell2); 
+            }
 
         }
 
@@ -102,22 +196,4 @@ public class Problem02{
 
 
     }
-
-    public static String subOperator(String action, String cell1, String cell2) {
-        int n = 0;
-        if (action.length == 4) {
-            n = Integer.parseInt((""+action.charAt(3)));
-            if(action.contains("ADD")){
-                return
-            }
-            if(action.contains("SUB")){
-
-            }
-
-        }
-
-
-    }
-
-
 }
